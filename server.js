9968e3b964d80dea816547dd0081953984925487
Glitch (pajsen-portfolio) <none>
2020-09-10 14:16:35 +0000

    Latest verion

--- server.js
+++ server.js
@@ -1,25 +1,52 @@
-var express = require("express");
-var app = express();
+
+
+// server.js
+// where your node app starts
+
+
+const express = require("express");
+const app = express();
+
+
+const api = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+
 
 app.use(express.static("public"));
 
-app.get("/", (request, res) => {res.sendFile(__dirname + "/views/index.html"); });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname +  "/views/index.html");
+});
 
-//app.get("/contact", (req, res) => { res.sendFile(__dirname + "/views/contact.html"); });
-                                  
-//app.get("/about", (req, res) => { res.sendFile(__dirname + "/views/about.html"); });
+app.get("/data", (req, res) => {
+  res.sendFile(__dirname +  "/views/get-data.html");
+});
 
-//app.get("/sdagdggergdsthjdudghs5r784887786846c8g49sdz4fhgs6894ghdf641dswhgj", (req, res) => { res.sendFile(__dirname + "/views/google.html"); });
+app.get("/tetris", (req, res) => {
+  res.sendFile(__dirname + "/views/tetris.html");
+});
 
- 
-/*app.get("", (req, res) => { res.sendFile(__dirname + ""); });*/
 
-/*app.get("", (req, res) => { res.sendFile(__dirname + ""); });*/
-
-app.use(function(req, res, next){ 
+app.use(function (req, res, next) {
+  //res.status(404).send("Sorry can't find that!")
   res.sendFile(__dirname + "/views/404.html"); 
+});
+
+
+app.get("/api", (request, res) => {
+
+  res.json(api);
+  console.log(api); 
+  res.sendFile("views/index.html")
 });
 
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
+
 });
+
+
